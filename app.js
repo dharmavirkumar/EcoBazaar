b961a6b24ba0954c43ae09b8ecd09215f75a10fb
Dharmavirkumar <dharmavirkumar1215@gmail.com>
2026-04-04 19:14:26 +0530

    fix the bug

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.use('/', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
